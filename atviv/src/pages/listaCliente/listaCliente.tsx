39b2ba4d65d3414aac6c1413b19aeca6d3b5c4af
Mariana Tamay <maritamay4@gmail.com>
2022-11-08 18:51:13 -0300

    Ajustes na lista de clientes

--- atviv/src/pages/listaCliente/listaCliente.tsx
+++ atviv/src/pages/listaCliente/listaCliente.tsx
@@ -8,36 +8,39 @@ export default function ListaCliente(){
 
             <div className="cadastro">
                 <h1>Lista de clientes</h1>
-                <Table>
-                    <thead>
-                        <tr>
-                        <th>#</th>
-                        <th>Nome</th>
-                        <th>Nome social</th>
-                        <th>Telefone</th>
-                        <th></th>
-                        <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                            <td><Button variant="outline-success" className="outline">Editar</Button></td>
-                            <td><Button variant="outline-danger" className="outline">Excluir</Button></td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                            <td><Button variant="outline-success" className="outline">Editar</Button></td>
-                            <td><Button variant="outline-danger" className="outline">Excluir</Button></td>
-                        </tr>
-                    </tbody>
-                </Table>
+                <div>
+                    <Table>
+                        <thead>
+                            <tr>
+                            <th>Tipo</th>
+                            <th>Nome</th>
+                            <th>Nome social</th>
+                            <th>Telefone</th>
+                            <th></th>
+                            <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Titular</td>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                                <td><Button variant="outline-success" className="outline">Editar</Button></td>
+                                <td><Button variant="outline-danger" className="outline">Excluir</Button></td>
+                            </tr>
+                            <tr>
+                                <td>Dependente</td>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                                <td><Button variant="outline-success" className="outline">Editar</Button></td>
+                                <td><Button variant="outline-danger" className="outline">Excluir</Button></td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </div>
+                
             </div>
         </>
     )
