a3e1828294b5b1f5bb9baf7244ee59be754f2fdd
Mariana Tamay <maritamay4@gmail.com>
2022-11-08 18:51:00 -0300

    Adição de tabs na página de cadastro de cliente

--- atviv/src/pages/cadCliente/cadastroCliente.tsx
+++ atviv/src/pages/cadCliente/cadastroCliente.tsx
@@ -1,5 +1,7 @@
-import { Button, Form, InputGroup } from "react-bootstrap";
+import { Button, Form, InputGroup, Tabs, Tab } from "react-bootstrap";
+import Dependente from "../../components/dependente";
 import NavBar from "../../components/navbar";
+import Titular from "../../components/titular";
 
 import "./cadcli.css"
 
@@ -12,84 +14,21 @@ export default function CadCliente(){
 
                 <hr/>
 
-                <Form className="row">
-                    <Form.Group className="mb-3 col-6">
-                        <Form.Label>Nome completo</Form.Label>
-                        <Form.Control type="text" className="input-cadcli"/>
-                    </Form.Group>
-                    <Form.Group className="mb-3 col-6">
-                        <Form.Label>Nome social</Form.Label>
-                        <Form.Control type="text" className="input-cadcli"/>
-                    </Form.Group>
+                <Tabs
+                    defaultActiveKey="profile"
+                    id="uncontrolled-tab-example"
+                    className="mb-3"
+                >
+                    <Tab eventKey="home" title="Titular">
+                        <Titular/>
+                    </Tab>
+                    <Tab eventKey="profile" title="Dependente">
+                        <Dependente/>
+                    </Tab>
+                </Tabs>
 
-                    <h3>Documentos</h3>
-                    <Form.Group className="mb-3 col-2">
-                        <Form.Label>Tipo de documento</Form.Label>
-                        <Form.Select className="input-cadcli">
-                            <option>Tipo de documento</option>
-                            <option>CPF</option>
-                            <option>RG</option>
-                            <option>Passaporte</option>
-                        </Form.Select>
-                    </Form.Group>
-                    <Form.Group className="mb-3 col-5">
-                        <Form.Label>Data de expedição</Form.Label>
-                        <Form.Control type="text" className="input-cadcli"/>
-                    </Form.Group>
-                    <Form.Group className="mb-3 col-5">
-                        <Form.Label>Número</Form.Label>
-                        <Form.Control type="text" className="input-cadcli"/>
-                    </Form.Group>
 
-                    <h3>Telefone</h3>
-                    <Form.Group className="mb-3 col-2">
-                        <Form.Label>DDD</Form.Label>
-                        <Form.Control type="text" className="input-cadcli"/>
-                    </Form.Group>
-                    <Form.Group className="mb-3 col-5">
-                        <Form.Label>Número do telefone</Form.Label>
-                        <Form.Control type="text" className="input-cadcli"/>
-                    </Form.Group>
-
-                    <Form.Group className="mb-3 col-5">
-                        <Form.Label>Tipo de cliente</Form.Label>
-                        <Form.Select className="input-cadcli">
-                            <option>Modalidade de cliente</option>
-                            <option>Titular</option>
-                            <option>Dependente</option>
-                        </Form.Select>
-                    </Form.Group>
-                </Form>
-
-                <hr/>
-
-                <Form className="row">
-                <h3>Endereço</h3> 
-                    <Form.Group className="mb-3 col-6">
-                        <Form.Label>Rua</Form.Label>
-                        <Form.Control type="text" className="input-cadcli"/>
-                    </Form.Group>
-                    <Form.Group className="mb-3 col-6">
-                        <Form.Label>Bairro</Form.Label>
-                        <Form.Control type="text" className="input-cadcli"/>
-                    </Form.Group>
-                    <Form.Group className="mb-3 col-4">
-                        <Form.Label>País</Form.Label>
-                        <Form.Control type="text" className="input-cadcli"/>
-                    </Form.Group>
-                    <Form.Group className="mb-3 col-4">
-                        <Form.Label>Estado</Form.Label>
-                        <Form.Control type="text" className="input-cadcli"/>
-                    </Form.Group>
-                    <Form.Group className="mb-3 col-4">
-                        <Form.Label>Cidade</Form.Label>
-                        <Form.Control type="text" className="input-cadcli"/>
-                    </Form.Group>
-                </Form>
-
-                <hr/>
-
-                <Button size="lg" className="botao-cadcli">Cadastrar!</Button>
+                
             </div>
         </>
     )
